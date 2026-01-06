8eadbdcde06ea7c9aac56ef0b851fcc973eae359
roshi1205 <roshinivadivel1205@gmail.com>
2026-01-06 19:48:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import BookList from "./components/BookList";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Library Book Management System</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
